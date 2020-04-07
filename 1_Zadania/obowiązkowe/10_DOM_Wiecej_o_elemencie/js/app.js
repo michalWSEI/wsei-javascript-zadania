eb02797628b5567846527879d1335129ea61b821
Michal Jaracz <michal.jaracz@stepstone.com>
2020-04-07 19:02:41 +0200

    10_DOM: exc 1, 2

--- 1_Zadania/obowiązkowe/10_DOM_Wiecej_o_elemencie/js/app.js
+++ 1_Zadania/obowiązkowe/10_DOM_Wiecej_o_elemencie/js/app.js
@@ -18,7 +18,34 @@ document.addEventListener("DOMContentLoaded", function () {
 	// exc1
 	const ex1Element = document.querySelector('.ex1');
 	const edge = document.querySelector('.edge');
-	const chromeElement = document.querySelector('.chrome');
-	const firefoxElement = document.querySelector('.firefox');
-	edge.style
+	const chrome = document.querySelector('.chrome');
+	const firefox = document.querySelector('.firefox');
+	const links = document.querySelectorAll('.exercise > div > a[href*="www"]');
+	edge.style.backgroundImage = "../assets/img/edge.png";
+	chrome.style.backgroundImage = "../assets/img/chrome.png";
+	firefox.style.backgroundImage = "../assets/img/firefox.png";
+	links.forEach((link, index) => {
+		if (index === 0) {
+			link.innerHTML = "Chrome";
+			link.attributes[0].value = "https://www.google.com/intl/pl_pl/chrome/";
+		}
+		if (index === 1) {
+			link.innerHTML = "Edge";
+			link.attributes[0].value = "https://www.microsoft.com/pl-pl/edge";
+		}
+		if (index === 2) {
+			link.innerHTML = "Firefox";
+			link.attributes[0].value = "https://mozilla.org/pl/firefox/new/";
+		}
+	});
+	chrome.style.width = "100 px";
+	// exc2
+	const nameElem = document.querySelector('#name');
+	const favColor = document.querySelector('#fav_color');
+	const favMeal = document.querySelector('#fav_meal');
+	nameElem.innerHTML = 'Michał Jaracz';
+	favColor.innerHTML = 'Turquoise Blue';
+	favMeal.innerHTML = 'Zapiekany schab ze śliwka';
+	// exc3
+
 });
